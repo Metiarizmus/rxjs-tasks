65b277435677bc59ba93565bd54332f1bb38b089
Nikolai Nagornykh <mikalai.nahornykh@innowise-group.com>
2022-11-28 17:43:45 +0300

    clear master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { OneComponent } from './shared/components/one/one.component';
-import {TwoComponent} from "./shared/components/two/two.component";
-import { ThreeComponent } from './shared/components/three/three.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OneComponent,
-    TwoComponent,
-    ThreeComponent
   ],
   imports: [
     BrowserModule,
